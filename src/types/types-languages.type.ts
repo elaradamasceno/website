87ee03b565676e292ec6dc25cb3bb1dfbc6bd26b
Elara Damasceno <elaradamasceno@MacBook-Pro-de-Elara.local>
2024-03-16 13:45:06 -0300

    developed resume download page

---
+++ src/types/types-languages.type.ts
@@ -0,0 +1,12 @@
+export const ResumeDownload = {
+  portuguese: {
+    name: 'Elara Damasceno.doc',
+    title: 'PT-BR',
+    link: 'https://docs.google.com/document/d/1z3AMreoQGwD-Is6pCHKq9YEy6AsD_J_r/edit?usp=sharing&ouid=106609027922325216850&rtpof=true&sd=true'
+  },
+  english: {
+    name: 'Damasceno Elara.doc',
+    title: 'EN-US',
+    link: 'https://docs.google.com/document/d/10tVfOak_v-KCjml8kSVBpiHIY2NH2xbY/edit?usp=sharing&ouid=106609027922325216850&rtpof=true&sd=true'
+  }
+}
